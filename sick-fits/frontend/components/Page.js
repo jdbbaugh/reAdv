73cea8468736f63c99461337bd709c6f16636961
jdbbaugh <jbaugh78@msn.com>
2019-06-25 07:32:05 -0500

    quik save

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
 
+const MyButton = styled.button`
+  background: red;
+  font-size: ${props => (props.huge ? '100px' : '50px')};
+  .rock_on {
+    font-size: 100px;
+  }
+  `;
+
 export default class Page extends Component {
   render() {
     return (
