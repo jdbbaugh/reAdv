b632ffda3d8d4ad39078ac1fc809b9c0ed3f3c95
jdbbaugh <jbaugh78@msn.com>
2019-07-22 07:21:13 -0500

    start vid 16 for realz

--- sick-fits/backend/src/db.js
+++ sick-fits/backend/src/db.js
@@ -1,5 +1,5 @@
 // This file connects to the remote prisma DB and gives us the ability to query it with JS
-const { Prisma } = require('prisma-binding')
+const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
